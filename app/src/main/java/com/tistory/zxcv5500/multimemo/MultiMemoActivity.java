e2456b584de9d1f62d70eae8cc4903028a4d9623
zxcv5500 <zxcv5500@gmail.com>
2018-05-28 17:01:44 +0900

    Initial commit

---
+++ app/src/main/java/com/tistory/zxcv5500/multimemo/MultiMemoActivity.java
@@ -0,0 +1,13 @@
+package com.tistory.zxcv5500.multimemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MultiMemoActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_multi_memo);
+	}
+}
