a1356b659264cefa8750e8d01b5986daaa2bbff3
zxcv5500 <zxcv5500@gmail.com>
2018-05-28 18:32:37 +0900

    main xml작성 및 리소스 추가

--- app/src/main/java/com/tistory/zxcv5500/multimemo/MultiMemoActivity.java
+++ app/src/main/java/com/tistory/zxcv5500/multimemo/MultiMemoActivity.java
@@ -8,6 +8,6 @@ public class MultiMemoActivity extends AppCompatActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_multi_memo);
+		setContentView(R.layout.main);
 	}
 }
