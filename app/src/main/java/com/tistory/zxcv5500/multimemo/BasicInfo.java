db0344d40e84dd9465182ff5652b1d518ad525bf
zxcv5500 <zxcv5500@gmail.com>
2018-05-29 01:43:27 +0900

    앱의 파일 저장 경로를 설정하는 BasicInfo 작성

---
+++ app/src/main/java/com/tistory/zxcv5500/multimemo/BasicInfo.java
@@ -0,0 +1,33 @@
+package com.tistory.zxcv5500.multimemo;
+
+/**
+ * Created by zxcv5500 on 2018-05-29.
+ */
+
+public class BasicInfo {
+
+	/**
+	 * 사진 저장 위치
+	 */
+	public static final String FOLDER_PHOTO = "/sdcard/MultimediaMemo/photo/";
+
+	/**
+	 * 동영상 저장 위치
+	 */
+	public static final String FOLDER_VIDEO = "/sdcard/MultimediaMemo/video";
+
+	/**
+	 * 녹음 저장 위치
+	 */
+	public static final String FOLDER_VOICE = "/sdcard/MultimediaMemo/voice";
+
+	/**
+	 * 손글씨 저장 위치
+	 */
+	public static final String FOLDER_HANDWRITING = "/sdcard/MultimediaMemo/handwriting";
+
+	/**
+	 * 미디어 포맷
+	 */
+	public static final String URI_MEDIA_FORMAT = "content://media";
+}
